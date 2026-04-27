99d894de0748ed99b4c7b79ba181d04da1a7b7ed
Antonio Killebrew <antoniokillebrew100@gmail.com>
2026-04-27 13:23:42 -0500

    Create fps for game window.

--- src/project.py
+++ src/project.py
@@ -7,9 +7,14 @@ GAME_HEIGHT = 512
 pygame.init()
 window = pygame.display.set_mode((GAME_WIDTH,GAME_HEIGHT))
 pygame.display.set_caption("Uhura")
+clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
+    pygame.display.update()
+    clock.tick(60)
+    
