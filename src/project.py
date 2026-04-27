7148b795f9afed0585dea4a0cd3d0d4adf883f69
Antonio Killebrew <antoniokillebrew100@gmail.com>
2026-04-27 13:28:41 -0500

    Draw a background fill for the window.

--- src/project.py
+++ src/project.py
@@ -9,12 +9,15 @@ window = pygame.display.set_mode((GAME_WIDTH,GAME_HEIGHT))
 pygame.display.set_caption("Uhura")
 clock = pygame.time.Clock()
 
+def draw():
+    window.fill((20,18,167))
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
+    draw()
     pygame.display.update()
     clock.tick(60)
-    
