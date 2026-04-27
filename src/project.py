0685f20b54b7effd30537cf7cc834955b2dae6ad
Antonio Killebrew <antoniokillebrew100@gmail.com>
2026-04-27 13:16:41 -0500

    Create a game window with a game loop to listen for events.

--- src/project.py
+++ src/project.py
@@ -1,1 +1,11 @@
 import pygame
+from sys import exit
+
+pygame.init()
+window = pygame.display.set_mode((500,500))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
