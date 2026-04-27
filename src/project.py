69b9cdc86a64f4f30673b87a235a29b64bffbecb
Antonio Killebrew <antoniokillebrew100@gmail.com>
2026-04-27 13:21:13 -0500

    Create game window variables and add a window caption.

--- src/project.py
+++ src/project.py
@@ -1,8 +1,12 @@
 import pygame
 from sys import exit
 
+GAME_WIDTH = 512
+GAME_HEIGHT = 512
+
 pygame.init()
-window = pygame.display.set_mode((500,500))
+window = pygame.display.set_mode((GAME_WIDTH,GAME_HEIGHT))
+pygame.display.set_caption("Uhura")
 
 while True:
     for event in pygame.event.get():
