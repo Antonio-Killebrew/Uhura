e0c0f8394ad7fee83b3c22a02ca7ac34b4f3989a
Antonio Killebrew <antoniokillebrew100@gmail.com>
2026-04-27 20:18:45 -0500

    Replace rectangle player with a drawn image using megaman placeholder character image

--- src/project.py
+++ src/project.py
@@ -1,22 +1,35 @@
+import os
 import pygame
 from sys import exit
 
 GAME_WIDTH = 512
 GAME_HEIGHT = 512
 
-background_image = pygame.image.load("images/background.png")
+PLAYER_X = GAME_WIDTH/2
+PLAYER_Y = GAME_WIDTH/2
+PLAYER_WIDTH = 42
+PLAYER_HEIGHT = 48
+
+background_image = pygame.image.load(os.path.join("images","background.png"))
+player_image_right = pygame.image.load(os.path.join("images","megaman-right.png"))
+player_image_right = pygame.transform.scale(player_image_right,(PLAYER_WIDTH,PLAYER_HEIGHT))
 
 pygame.init()
 window = pygame.display.set_mode((GAME_WIDTH,GAME_HEIGHT))
 pygame.display.set_caption("Uhura")
 clock = pygame.time.Clock()
 
-player = pygame.Rect(150,150,50,50)
+class Player(pygame.Rect):
+    def __init__(self):
+        pygame.Rect.__init__(self, PLAYER_X, PLAYER_Y, PLAYER_WIDTH, PLAYER_HEIGHT)
+        self.image = player_image_right
+
+player = Player()
 
 def draw():
     window.fill((20,18,167))
-    window.blit(background_image,(0,0))
-    pygame.draw.rect(window,(2,239,238),player)
+    window.blit(background_image,(0,80))
+    window.blit(player.image,player)
 
 while True:
     for event in pygame.event.get():
