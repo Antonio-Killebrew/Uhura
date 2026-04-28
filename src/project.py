9b8156c7f7082a5443f85220ca34dc0932dd009b
Antonio Killebrew <antoniokillebrew100@gmail.com>
2026-04-27 22:20:27 -0500

    Optimize character print code and create code to update character sprite when moving/jumping left or right

--- src/project.py
+++ src/project.py
@@ -15,9 +15,15 @@ GRAVITY = 0.5
 PLAYER_VELOCITY_Y = -10
 FLOOR_Y = GAME_HEIGHT * 3/4
 
-background_image = pygame.image.load(os.path.join("images","background.png"))
-player_image_right = pygame.image.load(os.path.join("images","megaman-right.png"))
-player_image_right = pygame.transform.scale(player_image_right,(PLAYER_WIDTH,PLAYER_HEIGHT))
+def load_image(image_name, scale=None):
+    image = pygame.image.load(os.path.join("images",image_name))
+    if scale is not None:
+        image = pygame.transform.scale(image,scale)
+    return image
+
+background_image = load_image("background.png")
+player_image_right = load_image("megaman-right.png", (PLAYER_WIDTH, PLAYER_HEIGHT))
+player_image_left = load_image("megaman-left.png",(PLAYER_WIDTH, PLAYER_HEIGHT))
 
 pygame.init()
 window = pygame.display.set_mode((GAME_WIDTH,GAME_HEIGHT))
@@ -30,6 +36,14 @@ class Player(pygame.Rect):
         pygame.Rect.__init__(self, PLAYER_X, PLAYER_Y, PLAYER_WIDTH, PLAYER_HEIGHT)
         self.image = player_image_right
         self.velocity_y = 0
+        self.direction = "right"
+        self.jumping = False
+
+    def update_image(self):
+        if self.direction == "right":
+            self.image = player_image_right
+        elif self.direction == "left":
+            self.image = player_image_left
 
 player = Player()
 
@@ -39,10 +53,12 @@ def move():
 
     if player.y + player.height > FLOOR_Y:
         player.y = FLOOR_Y - player.height
+        player.jumping = False
 
 def draw():
     window.fill((20,18,167))
     window.blit(background_image,(0,80))
+    player.update_image()
     window.blit(player.image,player)
 
 while True:
@@ -52,15 +68,17 @@ while True:
             exit()
 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP] or keys[pygame.K_w]:
+    if keys[pygame.K_UP] or keys[pygame.K_w] and not player.jumping:
         player.velocity_y = PLAYER_VELOCITY_Y
+        player.jumping = True
 
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
         player.x = max(player.x - PLAYER_DISTANCE, 0)
+        player.direction = "left"
 
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         player.x = min(player.x + PLAYER_DISTANCE, GAME_WIDTH - player.width)
-
+        player.direction = "right"
 
     move()
     draw()
